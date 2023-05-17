961bbb0329d8b5d1ce561a3e884d0ea3fed019ae
Tharindu Umayanga <tharinduumayangat@gmail.com>
2023-05-18 00:44:17 +0530

    first comit

--- lib/Schedule.dart
+++ lib/Schedule.dart
@@ -10,33 +10,135 @@ class Schedule extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text('Schedule'),
-          backgroundColor: Color.fromARGB(181, 177, 255, 224),
+          backgroundColor: Color.fromRGBO(177, 190, 255, 75),
         ),
-        body: Row(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Expanded(
-              child: ElevatedButton(
-                onPressed: () {
-                  // TODO: Implement button 1 functionality
-                },
-                child: Text('Button 1'),
-              ),
+            Row(
+              children: [
+                Builder(builder: (context) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 100.0, horizontal: 100.0),
+                  );
+                }),
+                Builder(builder: (context) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 11.0, horizontal: 11.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/qa");
+                      },
+                      child: const SizedBox(
+                        height: 130,
+                        width: 130,
+                        child: Center(
+                          child: Text('QA'),
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color.fromRGBO(255, 177, 177, 50),
+                      ),
+                    ),
+                  );
+                }),
+              ],
             ),
-            Expanded(
-              child: ElevatedButton(
-                onPressed: () {
-                  // TODO: Implement button 2 functionality
-                },
-                child: Text('Button 2'),
-              ),
+            Row(
+              children: [
+                Builder(builder: (context) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 100.0, horizontal: 100.0),
+                  );
+                }),
+                Builder(builder: (context) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 11.0, horizontal: 11.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/qa");
+                      },
+                      child: const SizedBox(
+                        height: 130,
+                        width: 130,
+                        child: Center(
+                          child: Text('QA'),
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color.fromRGBO(255, 177, 177, 50),
+                      ),
+                    ),
+                  );
+                }),
+              ],
             ),
-            Expanded(
-              child: ElevatedButton(
-                onPressed: () {
-                  // TODO: Implement button 3 functionality
-                },
-                child: Text('Button 3'),
-              ),
+            Row(
+              children: [
+                Builder(builder: (context) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 100.0, horizontal: 100.0),
+                  );
+                }),
+                Builder(builder: (context) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 11.0, horizontal: 11.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/qa");
+                      },
+                      child: const SizedBox(
+                        height: 130,
+                        width: 130,
+                        child: Center(
+                          child: Text('QA'),
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color.fromRGBO(255, 177, 177, 50),
+                      ),
+                    ),
+                  );
+                }),
+              ],
+            ),
+            Row(
+              children: [
+                Builder(builder: (context) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 93.0, horizontal: 100.0),
+                  );
+                  
+                }),
+                Builder(builder: (context) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 11.0, horizontal: 11.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/qa");
+                      },
+                      child: const SizedBox(
+                        height: 130,
+                        width: 130,
+                        child: Center(
+                          child: Text('QA'),
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color.fromRGBO(255, 177, 177, 50),
+                      ),
+                    ),
+                  );
+                }),
+              ],
             ),
           ],
         ),
